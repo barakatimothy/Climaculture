b4edf947e1a68a67dab198854167e5e39eeaa99c
barakatimothy <timothybaraka39@gmail.com>
2023-10-23 11:37:45 +0300

    made the api endpoint for creating a new user and getting all users

--- server/app.py
+++ server/app.py
@@ -1,7 +1,40 @@
-from flask import Flask 
+from flask import Flask, render_template,jsonify,request
 from flask_sqlalchemy import SQLAlchemy
+from models import User
+
+my_url= 'https:localhost'
 
 app = Flask(__name__)
-app.config['DATABASE_URI'] = ''
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///app.db'  
 
 db = SQLAlchemy(app)
+
+@app.route('/')
+def home():
+    return "Welcome to the Weather App!"
+
+@app.route('/users', methods=['GET'])
+def get_users():
+   users = User.query.all()
+   user_list =[{'user_id': user.id ,'user_name':user.username, 'email':user.enail}for user in users]
+   return jsonify(user_list)
+@app.route('/users',methods=['POST'])
+def create_user():
+    data = request.get_json()
+    if 'username' in data and 'email' in data and 'password' in data:
+        new_user = User(username=data['username'], email=data['email'], password_hash=data['password'])
+        db.session.add(new_user)
+        db.session.commit()
+        return jsonify({'message': 'user created succesfully'})
+    else:
+        return jsonify({'error': 'Missing required data'})
+@app.route('/locations')
+def locations():
+    return "Location List"
+
+@app.route('/weather_data')
+def weather_data():
+    return "Weather Data List"
+
+if __name__ == '__main__':
+    app.run(debug=True)
--- server/models.py
+++ server/models.py
@@ -20,3 +20,47 @@ class User(db.Model):
 
     def __repr__(self):
         return f'<User {self.username}>'
+    
+
+class Location(db.Model):
+     __tablename__ = 'locations'
+
+     location_id = db.Column(db.Integer, primary_key=True)
+     name = db.Column(db.String(100), unique=True, nullable=False)
+     latitude = db.Column(db.Real)
+     longitude  = db.Column(db.Real)
+
+     def __init__ (self, name, latitude, longitude):
+        self.name = name 
+        self.latitude = latitude 
+        self.longitude = longitude
+
+     def __repr__(self):
+         return f'<name {self.name}>'
+     
+
+
+class WeatherData(db.Model):
+    __tablename__ = 'weather_data'
+   
+    data_id = db.Column(db.Integer, primary_key=True)
+    location_id = db.Column(db.Integer, ForeignKey('loction_id'))
+    date_time = db.Column(db.DateTime, default=datetime.utcnow)
+    temperature = db.Column(db.Real, nullable=False)
+    humidity = db.Column(db.Real, nullable=False)
+    wind_speed = db.Column(db.Real, nullable=False)
+    weather_condition = db.Column(db.String(100), nullable=False)
+
+
+    def __init__ (self, temperature, humidity, wind_speed, weather_condition):
+        self.temperature = temperature 
+        self.humidity = humidity 
+        self.wind_speed = wind_speed
+        self.weather_condition = weather_condition
+
+    def __repr__(self):
+        return f' <
+        temperature {self.temperature} 
+        humidity  {self.humidity} 
+        windspeed {self.wind_speed}>'
+
